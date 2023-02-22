0743fa814897f55dbc18006d81f6656b45eebc51
Rawllings Kiprop <rutojr01@gmail.com>
2023-02-22 22:10:21 +0300

    Completed assignment

---
+++ app/models/show.rb
@@ -0,0 +1,27 @@
+class Show < ActiveRecord::Base
+
+    def self.highest_rating
+        Show.maximum(:rating)
+    end
+
+    def self.most_popular_show
+        order(rating: :desc).first.name
+    end
+
+    def self.lowest_rating
+        Show.minimum(:rating)
+    end
+
+    def self.ratings_sum
+        Show.sum(:rating)
+    end
+
+    def self.popular_shows
+        Show.where("rating > ?", 5)
+    end
+
+    def self.shows_by_alphabetical_order
+        order(:name)
+        # self. order("name ASC")
+    end
+end
